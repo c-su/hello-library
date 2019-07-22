56cae97327293576ef3383d54bc60aad3b5378ea
c-su <m.y.like.music.10@gmail.com>
2019-07-22 16:42:17 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Vuex from 'vuex';
 import HelloWorld from './components/HelloWorld.vue';
 import HelloText from './components/HelloText.vue';
 
@@ -12,5 +13,12 @@ export default {
     for (const [name, c] of Object.entries(components)) {
       Vue.component(name, c);
     }
+  },
+  installStore() {
+    return new Vuex.Store({
+      state: {
+        edit: 'test',
+      },
+    });
   },
 };
