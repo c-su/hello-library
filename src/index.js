0e49c27bbc97aefa2cc2aaeed5d9e4937a1bbe12
c-su <m.y.like.music.10@gmail.com>
2019-07-22 17:15:13 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import store from './store';
 import HelloWorld from './components/HelloWorld.vue';
 import HelloText from './components/HelloText.vue';
+import HelloTextText from './components/HelloTextText.vue';
 
 export default {
   install(Vue) {
     const components = {
       HelloWorld,
       HelloText,
+      HelloTextText,
     };
 
     // eslint-disable-next-line no-restricted-syntax
